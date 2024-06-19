7fe26fd7de8c4ec95066df7d527d7b59db814351
Imista <benjamintec3455@gmail.com>
2024-06-19 14:03:22 -0600

    Get Methods

--- src/main.js
+++ src/main.js
@@ -1,29 +1,67 @@
 const API_KEY =
   "live_2KxgGFh3LyBGO3AI2SorYLHqpd9S8RLDg2NO0JZuR46B02ZxDvw7tnwSaf0VHOaR";
-const URL = "https://api.thecatapi.com/v1/images/search?limit=3";
-const imgContainer1 = document.querySelector("#img1");
-const imgContainer2 = document.querySelector("#img2");
-const imgContainer3 = document.querySelector("#img3");
-const buttonContainer = document.querySelector("button");
+const API_BASE_URL = "https://api.thecatapi.com/v1/";
 
-const getCat = async () => {
-  console.log("Start");
+//Random Cats
+const API_RANDOM_URL = API_BASE_URL + "images/search";
 
+const buttonRandom = document.querySelector("#button-random");
+
+const getRandomCat = async () => {
   try {
-    const res = await fetch(`${URL}&api_key=${API_KEY}`);
+    const res = await fetch(`${API_RANDOM_URL}?limit=3&api_key=${API_KEY}`);
     const data = await res.json();
+
+    const container = document.querySelector("#random-cats-images");
+
+    container.innerHTML = "";
+
+    data.forEach((cat) => {
+      const img = document.createElement("img");
+      img.src = cat.url;
+      img.alt = "Cat";
+      img.classList.add("cat-image");
+      container.appendChild(img);
+    });
+
     console.log(data);
-
-    imgContainer1.src = data[0].url;
-    imgContainer2.src = data[1].url;
-    imgContainer3.src = data[2].url;
-
-    console.log("End");
   } catch (error) {
     console.error("Error fetching cat:", error);
   }
 };
 
-getCat();
+getRandomCat();
 
-buttonContainer.addEventListener("click", getCat);
+buttonRandom.addEventListener("click", getRandomCat);
+
+//Favoritos
+
+const API_FAVORITES_URL = API_BASE_URL + "favourites";
+
+const getFavoriteCats = async () => {
+  try {
+    const res = await fetch(`${API_FAVORITES_URL}?api_key=${API_KEY}`);
+    // const res = await fetch(
+    //   `https://api.thecatapi.com/v1/favourites?api_key=${API_KEY}`
+    // );
+    const data = await res.json();
+
+    const container = document.querySelector("#favorites-cats-images");
+
+    container.innerHTML = "";
+
+    data.forEach((cat) => {
+      const img = document.createElement("img");
+      img.src = cat.url;
+      img.alt = "Cat";
+      img.classList.add("cat-image");
+      container.appendChild(img);
+    });
+
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching favorite cats:", error);
+  }
+};
+
+getFavoriteCats();
